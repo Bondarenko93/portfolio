4aa39589c68d3b0e8ca4465929411e5887179e09
Serhii Bondarenko <1flatron1993@gmail.com>
2022-09-10 19:47:06 +0000

    add content to home

--- pages/components/layout.tsx
+++ pages/components/layout.tsx
@@ -1,8 +1,17 @@
 import tw from "tailwind-styled-components"
 import { Header } from "./header"
+import { BackgroundMobile } from "./svg/background-mob"
 
 export const Layout = ({ children }) => {
-    return (<Container><Header /> {children}</Container>)
+    return (
+        <>
+            <Container><Header /> {children}
+                <div className='block absolute  svg top-0 w-full -z-50 '>
+                    <BackgroundMobile />
+                </div>
+            </Container>
+        </>
+    )
 }
 
 const Container = tw.div`lg:bg-[#0116276c] relative flex flex-col justify-between h-[96vh] md:h-[95vh] m-3 md:m-4  border-color border rounded-lg`
